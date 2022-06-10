547671c83a5dbbb08c6772484082fcf33145990b
Muhammad Vaid <molvy32@gmail.com>
2022-06-10 15:49:05 +0200

    Built basic UI

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends HookWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final searchController = useTextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Anime Finder'),
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: searchController,
+                  decoration:
+                      const InputDecoration(hintText: 'Enter image URL'),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(onPressed: () {}, child: const Text('Search'))
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Card(
+                    elevation: 2,
+                    child: Column(
+                      children: const [
+                        ListTile(
+                            leading: Text('Title'),
+                            title: Text('Attack on Titan')),
+                        ListTile(
+                            leading: Text('Title'),
+                            title: Text('Attack on Titan')),
+                        ListTile(
+                            leading: Text('Title'),
+                            title: Text('Attack on Titan'))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_anime/home.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(ProviderScope(observers: [Logger()], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,9 +15,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
       home: const HomeScreen(),
     );
   }
 }
+
+class Logger extends ProviderObserver {
+  @override
+  void didUpdateProvider(
+    ProviderBase provider,
+    Object? newValue,
+    Object? newValue2,
+    ProviderContainer PC,
+  ) {
+    print({
+      '''
+      "provider": "${provider.name ?? provider.runtimeType}",
+      "newValue": "$newValue",
+    
+      '''
+    });
+  }
+}
+
+//   "newValue2": "$newValue2"
+//   "PC": "$PC"
